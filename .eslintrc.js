9f6db049b41997cb4e6095b13e7e9cfeaed8e183
ub <kimub1204@naver.com>
2022-10-31 13:36:22 +0900

    init: eslint, prettier

---
+++ .eslintrc.js
@@ -0,0 +1,42 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2022: true,
+  },
+  extends: ["eslint:recommended", "airbnb-base", "plugin:prettier/recommended"],
+  parserOptions: {
+    ecmaVersion: "latest",
+    sourceType: "module",
+  },
+  plugins: ["prettier"],
+  rules: {
+    "linebreak-style": "off",
+    "no-empty-function": "off",
+    "object-shorthand": ["error", "always"],
+    "no-underscore-dangle": "off",
+    "spaced-comment": "off",
+    "one-var": "off",
+    "no-undef": "off",
+    "no-var": "off",
+    "no-void": "off",
+    "no-shadow": "off",
+    "no-unused-vars": "off",
+    "prefer-template": "off",
+    "no-useless-concat": "off",
+    "vars-on-top": "off",
+    "no-use-before-define": "off",
+    "prettier/prettier": [
+      "off",
+      {
+        endOfLine: "auto",
+      },
+      {
+        usePrettierrc: true,
+      },
+    ],
+    "no-console": "off",
+    "import/prefer-default-export": "off",
+    "func-names": "off",
+    "no-param-reassign": "off",
+  },
+};
---
+++ .prettierrc.js
@@ -0,0 +1,13 @@
+module.exports = {
+  printWidth: 80,
+  tabWidth: 2,
+  useTabs: false,
+  semi: true,
+  singleQuote: false,
+  jsxSingleQuote: true,
+  trailingComma: "all",
+  bracketSpacing: true,
+  bracketSameLine: true,
+  arrowParens: "avoid",
+  endOfLine: "auto",
+};
