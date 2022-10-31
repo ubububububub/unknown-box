39d68919b11a257c7b4610748a8260450d2adf61
sjoleee <cent7425@gmail.com>
2022-11-01 02:20:55 +0900

    feat: 필요한 페이지 작성 및 폴더 생성, 페이지별 라우팅 구현

---
+++ src/frontend/pages/AdminOrder/AdminOrder.js
@@ -0,0 +1,7 @@
+import Component from "../../core/Component.js";
+
+export class AdminOrder extends Component {
+  template() {
+    return `<div>관리자 주문관리 페이지입니다.</div>`;
+  }
+}
---
+++ src/frontend/pages/Recipt/Recipt.js
@@ -0,0 +1,7 @@
+import Component from "../../core/Component.js";
+
+export class Recipt extends Component {
+  template() {
+    return `<div>주문 완료 페이지입니다.</div>`;
+  }
+}
