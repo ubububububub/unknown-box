80e2fbaa0337da8d891ba4c581f120a32dcbb49e
sjoleee <cent7425@gmail.com>
2022-11-01 17:00:28 +0900

    feat: NotFound 페이지 레이아웃 작성

--- frontend/pages/NotFound/NotFound.js
+++ frontend/pages/NotFound/NotFound.js
@@ -2,6 +2,7 @@ import Component from "../../core/Component.js";
 
 export class NotFound extends Component {
   template() {
-    return `<div>Page not found</div>`;
+    return `<div>Page not found</div>
+    <button id="mainBtn">메인으로</button>`;
   }
 }
