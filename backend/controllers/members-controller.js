a9e8cc766d43f8d35b5029167e7f991a5c84e585
ub <kimub1204@naver.com>
2022-11-01 19:00:05 +0900

    refactor: service로직 controller로 분리 fixed #34

--- backend/controllers/members-controller.js
+++
@@ -1,8 +0,0 @@
-import { Router } from "express";
-import { userService } from "../services";
-
-const membersRouter = Router();
-
-membersRouter.get("/", userService.getUsers);
-
-export { membersRouter };
