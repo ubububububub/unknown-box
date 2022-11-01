da7c4b0a5c679ff0ea9bd745c5b289fd685f7fad
ub <kimub1204@naver.com>
2022-11-01 14:55:20 +0900

    feat: api 라우터 기능 추가

--- backend/app.js
+++ backend/app.js
@@ -1,6 +1,11 @@
+import "dotenv/config";
 import cors from "cors";
 import path from "path";
 import express from "express";
+import mongoose from "mongoose";
+import { router } from "./controllers";
+
+mongoose.connect(process.env.MONGODB_URL);
 
 const app = express();
 
@@ -9,6 +14,8 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
 app.use("/", express.static(path.resolve(__dirname, "../frontend")));
+
+app.use("/api", router);
 
 // 여기에도 __dirname 추가
 app.get("/*", (req, res) => {
