3027942aa871afb9551dbff96bf6bea7ed224060
ub <kimub1204@naver.com>
2022-11-01 18:59:08 +0900

    style: 주석 제거

--- backend/app.js
+++ backend/app.js
@@ -3,7 +3,7 @@ import cors from "cors";
 import path from "path";
 import express from "express";
 import mongoose from "mongoose";
-import { router } from "./controllers";
+import { router } from "./routers";
 import { errorHandler } from "./middlewares";
 
 mongoose.connect(process.env.MONGODB_URL);
@@ -18,7 +18,6 @@ app.use("/", express.static(path.resolve(__dirname, "../frontend")));
 
 app.use("/api", router);
 
-// 여기에도 __dirname 추가
 app.get("/*", (req, res) => {
   res.sendFile(path.resolve(__dirname, "../frontend", "index.html"));
 });
