cdffc8ec9483f37702bd0ee8aa4feab6a8c8f852
sjoleee <cent7425@gmail.com>
2022-11-04 05:08:44 +0900

    feat: 주문데이터 수정, 조회 api 작성

--- frontend/apis/index.js
+++ frontend/apis/index.js
@@ -25,3 +25,31 @@ export async function postLogin(data) {
     console.dir(err);
   }
 }
+
+export async function getShippingInfo(orderId) {
+  try {
+    const response = await fetch(`http://localhost:8080/api/order/${orderId}`, {
+      method: "GET",
+      headers: {
+        "Content-Type": "application/json"
+      }
+    });
+    return await response.json();
+  } catch (err) {
+    console.dir(err);
+  }
+}
+
+export async function postShippingInfo(data, orderId) {
+  try {
+    await fetch(`http://localhost:8080/api/order/${orderId}`, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify(data)
+    });
+  } catch (err) {
+    console.dir(err);
+  }
+}
