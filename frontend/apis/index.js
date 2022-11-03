b6219d13355cd700c45bc742cdb16c7529fbece0
sjoleee <cent7425@gmail.com>
2022-11-03 20:26:11 +0900

    feat: 로그아웃 기능 구현. 임시로 로그인페이지에 버튼 생성, gnb로 이동 예정

--- frontend/apis/index.js
+++ frontend/apis/index.js
@@ -1,6 +1,6 @@
 export async function postSignIn(data) {
   try {
-    const response = await fetch("http://localhost:8080/api/join", {
+    await fetch("http://localhost:8080/api/join", {
       method: "POST",
       headers: {
         "Content-Type": "application/json"
@@ -11,14 +11,29 @@ export async function postSignIn(data) {
     console.dir(err);
   }
 }
+
 export async function postLogin(data) {
   try {
-    const response = await fetch("http://localhost:8080/api/login", {
+    await fetch("http://localhost:8080/api/login", {
       method: "POST",
       headers: {
         "Content-Type": "application/json"
       },
       body: JSON.stringify(data)
+    });
+  } catch (err) {
+    console.dir(err);
+  }
+}
+
+export async function getLogout() {
+  try {
+    await fetch("http://localhost:8080/api/logout").then(() => {
+      function deleteCookie(token) {
+        document.cookie = token + "=; expires=Thu, 01 Jan 1970 00:00:01 GMT;";
+      }
+      deleteCookie("accessToken");
+      deleteCookie("refreshToken");
     });
   } catch (err) {
     console.dir(err);
--- frontend/components/LoginForm/LoginForm.js
+++ frontend/components/LoginForm/LoginForm.js
@@ -1,4 +1,4 @@
-import { postLogin } from "../../apis/index.js";
+import { getLogout, postLogin } from "../../apis/index.js";
 import Component from "../../core/Component.js";
 import { emailValidation, passwordValidation, qs } from "../../utils/index.js";
 
@@ -11,7 +11,8 @@ class LoginForm extends Component {
               <input type="password" id="password" name="password"/>
               <input type="submit" id="loginBtn" value="로그인"/>
             </form>
-            <button id="signInBtn">회원가입하기</button>`;
+            <button id="signInBtn">회원가입하기</button>
+            <button id="logoutBtn">로그아웃하기</button>`;
   }
 
   setEvent() {
@@ -22,9 +23,18 @@ class LoginForm extends Component {
     qs("#loginForm").addEventListener("submit", e => {
       this.handleLogin(e);
     });
+
+    qs("#logoutBtn").addEventListener("click", e => {
+      this.handleLogout(e);
+    });
   }
 
-  handleLogin() {
+  handleLogout(e) {
+    e.preventDefault();
+    getLogout();
+  }
+
+  handleLogin(e) {
     e.preventDefault();
     const [email, password] = Array.from(e.target).map(item => item.value);
 
