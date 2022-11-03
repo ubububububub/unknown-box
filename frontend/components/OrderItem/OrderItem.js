871f2a2c41283fbc3b76ab6d485af6f8dac953fe
nahyun ko <wartet2@gmail.com>
2022-11-03 06:02:21 +0000

    feat: AdminOrder 페이지 레이아웃 작성

---
+++ frontend/components/OrderItem/OrderItem.js
@@ -0,0 +1,38 @@
+import Component from "../../core/Component.js";
+
+export default class OrderItem extends Component {
+    
+    template () {
+        const { orderId, orderTime, orderProducts, orderState } = this.props;
+        return `
+            <tr id="order-item">
+                <td>${orderId}</td>
+                <td>${orderTime}</td>
+                <td>
+                    ${orderProducts[0].productInfo.productName}
+                    ${orderProducts.length > 1 ? `외 ${orderProducts.length-1}개` : ''}
+                </td>
+                <td>
+                    ${orderProducts.reduce((total, { productInfo, quantity }) => {
+                            return total += productInfo.price * quantity
+                    }, 0)
+                    .toLocaleString()}원
+                </td>
+                <td>${orderState}</td>
+                <td>
+                    <button type="button">주문수정</button>
+                </td>
+                <td>
+                    <button type="button">주문취소</button>
+                </td>
+                <td>
+                    <button type="button">주문삭제</button>
+                </td>
+            </tr>
+        `;
+    }
+
+    render () {
+        this.target.insertAdjacentHTML('beforeend', this.template());
+    }
+}
--- frontend/pages/AdminOrder/AdminOrder.js
+++ frontend/pages/AdminOrder/AdminOrder.js
@@ -1,7 +1,65 @@
 import Component from "../../core/Component.js";
+import Navbar from "../../components/Navbar/Navbar.js";
+import OrderList from "../../components/OrderList/OrderList.js";
+import { qs } from "../../utils/index.js";
+import { ADMIN_PAGE_NAV } from "../../const/index.js";
 
 export class AdminOrder extends Component {
-  template() {
-    return `<div>관리자 주문관리 페이지입니다.</div>`;
-  }
+      
+      template () {
+        return `
+          <ul id="order-nav"></ul>
+            <table id="order-list-container"></table>
+        `;
+      }
+    
+      mounted () {
+        const mockData = [
+          {
+            orderId: 1,
+            orderTime: "22-10-10",
+            orderState: "배송중",
+            orderProducts: [
+              {
+                productInfo: { productId: 1, productName: "콜라", price: "1000" },
+                quantity: "1"
+              },
+              {
+                productInfo: { productId: 2, productName: "사이다", price: "2000" },
+                quantity: "2"
+              },
+              {
+                productInfo: { productId: 4, productName: "주스", price: "1000" },
+                quantity: "2"
+              },
+              {
+                productInfo: { productId: 1,productName: "몬스터",price: "3000" },
+                quantity: "10"
+              }
+            ]
+          },
+          {
+            orderId: 2,
+            orderTime: "22-10-11",
+            orderState: "배송완료",
+            orderProducts: [
+              {
+                productInfo: { productId: 4, productName: "돼지고기", price: "8000" },
+                quantity: "1"
+              },
+              {
+                productInfo: { productId: 5, productName: "소고기", price: "50000" },
+                quantity: "2"
+              },
+              {
+                productInfo: { productId: 6, productName: "닭고기", price: "10000" },
+                quantity: "3"
+              }
+            ]
+          }
+        ];
+    
+        new Navbar(qs('#order-nav'), ADMIN_PAGE_NAV);
+        new OrderList(qs('#order-list-container'), mockData);
+    }
 }
