4e701a32dda52e6b7f2aeb17b5ca4c5ddebcc891
sjoleee <cent7425@gmail.com>
2022-11-04 22:00:30 +0900

    feat: Toast 컴포넌트 작성

---
+++ frontend/components/Toast/Toast.js
@@ -0,0 +1,31 @@
+import { qs } from "../../utils/index.js";
+
+class Toast {
+  constructor(text) {
+    this.text = text || "확인";
+    this.render();
+  }
+
+  render() {
+    if (!qs(".toast-container")) {
+      this.container = document.createElement("div");
+      this.container.className = "toast-container";
+      document.body.appendChild(this.container);
+    } else {
+      this.container = qs(".toast-container");
+    }
+    this.toast = document.createElement("div");
+    this.toast.className = "toast";
+    this.container.insertAdjacentElement("afterbegin", this.toast);
+
+    const text = document.createElement("span");
+    text.innerText = this.text;
+    this.toast.appendChild(text);
+
+    setTimeout(() => {
+      this.toast.parentNode?.removeChild(this.toast);
+    }, 2000);
+  }
+}
+
+export default Toast;
