842546da2041c55a379359c652bc9d52674229eb
Salmambo <zxcvsqwe@gmail.com>
2022-11-04 23:44:47 +0900

    feat:error-handler status default or 500

--- backend/middlewares/error-handler.js
+++ backend/middlewares/error-handler.js
@@ -1,7 +1,7 @@
 const errorHandler = (error, req, res, next) => {
   console.log("\x1b[33m%s\x1b[0m", error.stack);
 
-  res.status(400).json({ result: "error", reason: error.message });
+  res.status(error.status || 500).json({ error: error.message });
 };
 
 export { errorHandler };
