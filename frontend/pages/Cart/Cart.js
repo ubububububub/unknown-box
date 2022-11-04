ca209e728cc213bb26676e711bb86ba17e618302
ub <kimub1204@naver.com>
2022-11-04 14:01:59 +0900

    style: Cart페이지 결제정보 목 데이터 추가

--- frontend/pages/Cart/Cart.js
+++ frontend/pages/Cart/Cart.js
@@ -1,5 +1,5 @@
 import Component from "../../core/Component.js";
-import { PaymentInfo } from "../../components/PaymentInfo/PaymentInfo.js";
+import PaymentInfo from "../../components/PaymentInfo/PaymentInfo.js";
 import { qs } from "../../utils/index.js";
 
 export class Cart extends Component {
@@ -7,9 +7,23 @@ export class Cart extends Component {
     return `<div class="container">장바구니 페이지입니다.</div>`;
   }
 
-  render() {
+  mounted() {
     new PaymentInfo(qs(".container"), [
-      { name: "MATIN FLEECE ZIP UP JUMPER IN BLACK", price: 198000 }
+      {
+        name: "MATIN FLEECE ZIP UP JUMPER IN BLACK",
+        price: 198000,
+        quantity: 1
+      },
+      {
+        name: "NJ1DN82B 여성 눕시 하이브리드 다운 볼 자켓",
+        price: 249000,
+        quantity: 1
+      },
+      {
+        name: "[유아인 착용 상품] 리버서블 플리스 숏 패딩 재킷 [블랙/크림] ",
+        price: 89900,
+        quantity: 1
+      }
     ]);
   }
 }
