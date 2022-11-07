5a08e61593672d4c44a324623990c4786bece61c
nahyyun <nahyuun@naver.com>
2022-11-07 16:49:03 +0900

    feat: 관리자 API 수정

--- frontend/apis/index.js
+++ frontend/apis/index.js
@@ -83,3 +83,191 @@ export async function deleteOrderInfo(orderId) {
     console.dir(err);
   }
 }
+
+export async function getCategoryList() {
+  try {
+    const response = await fetch("http://localhost:8080/api/admin/category", {
+      method: "GET",
+      headers: {
+        "Content-Type": "application/json"
+      }
+    });
+    return response.json();
+  } catch (err) {
+    console.dir(err);
+  }
+}
+
+export async function addCategory(data) {
+  try {
+    const response = await fetch(`http://localhost:8080/api/admin/category`, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify(data)
+    });
+    return response.json();
+  } catch (err) {
+    console.dir(err);
+  }
+}
+
+export async function editCategory(id) {
+  try {
+    const response = await fetch(
+      `http://localhost:8080/api/admin/category/${id}`,
+      {
+        method: "PUT",
+        headers: {
+          "Content-Type": "application/json"
+        },
+        body: JSON.stringify(data)
+      }
+    );
+    return response.json();
+  } catch (err) {
+    console.dir(err);
+  }
+}
+
+export async function deleteCategory(id) {
+  try {
+    await fetch(`http://localhost:8080/api/admin/category/${id}`, {
+      method: "DELETE",
+      headers: {
+        "Content-Type": "application/json"
+      }
+    });
+  } catch (err) {
+    console.dir(err);
+  }
+}
+
+export async function getProductList() {
+  try {
+    const response = await fetch("http://localhost:8080/api/admin/product", {
+      method: "GET",
+      headers: {
+        "Content-Type": "application/json"
+      }
+    });
+    return await response.json();
+  } catch (err) {
+    console.dir(err);
+  }
+}
+
+export async function addProduct(data) {
+  try {
+    await fetch(`http://localhost:8080/api/admin/product`, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify(data)
+    });
+  } catch (err) {
+    console.dir(err);
+  }
+}
+
+export async function getProductDetail(id) {
+  try {
+    const response = await fetch(`http://localhost:8080/api/product/${id}`, {
+      method: "GET",
+      headers: {
+        "Content-Type": "application/json"
+      }
+    });
+    return response.json();
+  } catch (err) {
+    console.dir(err);
+  }
+}
+
+export async function editProduct(id, data) {
+  try {
+    await fetch(`http://localhost:8080/api/admin/product/${id}`, {
+      method: "PUT",
+      headers: {
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify(data)
+    });
+  } catch (err) {
+    console.dir(err);
+  }
+}
+
+export async function deleteProduct(id) {
+  try {
+    await fetch(`http://localhost:8080/api/admin/product/${id}`, {
+      method: "DELETE",
+      headers: {
+        "Content-Type": "application/json"
+      }
+    });
+  } catch (err) {
+    console.dir(err);
+  }
+}
+
+export async function getOrderList() {
+  try {
+    const response = await fetch(`http://localhost:8080/api/admin/order`, {
+      method: "GET",
+      headers: {
+        "Content-Type": "application/json"
+      }
+    });
+    return response.json();
+  } catch (err) {
+    console.dir(err);
+  }
+}
+
+export async function getOrderDetail(id) {
+  try {
+    const response = await fetch(`http://localhost:8080/api/order/${id}`, {
+      method: "GET",
+      headers: {
+        "Content-Type": "application/json"
+      }
+    });
+    return response.json();
+  } catch (err) {
+    console.dir(err);
+  }
+}
+
+export async function editOrder(id, data) {
+  try {
+    const response = await fetch(
+      `http://localhost:8080/api/admin/order/${id}`,
+      {
+        method: "PUT",
+        headers: {
+          "Content-Type": "application/json"
+        },
+        body: JSON.stringify(data)
+      }
+    );
+    return response.json();
+  } catch (err) {
+    console.dir(err);
+  }
+}
+
+export async function deleteOrder(id) {
+  try {
+    await fetch(`http://localhost:8080/api/admin/order/${id}`, {
+      method: "DELETE",
+      headers: {
+        "Content-Type": "application/json"
+      }
+    });
+  } catch (err) {
+    console.dir(err);
+  }
+}
