b87635f5032b83c8cd424154c16127e5c38d1634
sjoleee <cent7425@gmail.com>
2022-11-09 01:04:57 +0900

    feat: Toast 컴포넌트 스타일 작성 및 유효성 검사 alert 대체

--- frontend/utils/index.js
+++ frontend/utils/index.js
@@ -1,3 +1,5 @@
+import Toast from "../components/Toast/Toast.js";
+
 export function qs(selector) {
   return document.querySelector(selector);
 }
@@ -11,11 +13,11 @@ export function emailValidation(email) {
     /^[0-9a-zA-Z]([-_.]?[0-9a-zA-Z])*@[0-9a-zA-Z]([-_.]?[0-9a-zA-Z])*.[a-zA-Z]{2,3}$/i;
 
   if (!email) {
-    alert("이메일 주소를 입력해주세요.");
+    new Toast("이메일 주소를 입력해주세요.");
     return false;
   }
   if (!RegExp.test(email)) {
-    alert("올바른 이메일 형식이 아닙니다.");
+    new Toast("올바른 이메일 형식이 아닙니다.");
     return false;
   }
 
@@ -26,11 +28,11 @@ export function passwordValidation(password, passwordConfirm) {
   const RegExp = /^(?=.*[a-zA-Z])(?=.*[!@#$%^*+=-])(?=.*[0-9]).{8,16}$/;
 
   if (!password) {
-    alert("비밀번호를 입력해주세요.");
+    new Toast("비밀번호를 입력해주세요.");
     return false;
   }
   if (!RegExp.test(password)) {
-    alert("비밀번호는 8~16자의 영문자+숫자+특수문자로 입력해주세요.");
+    new Toast("비밀번호는 8~16자의 영문자+숫자+특수문자로 입력해주세요.");
     return false;
   }
 
@@ -39,11 +41,11 @@ export function passwordValidation(password, passwordConfirm) {
 
 export function passwordConfirmValidation(password, passwordConfirm) {
   if (!passwordConfirm) {
-    alert("비밀번호 확인을 입력해주세요.");
+    new Toast("비밀번호 확인을 입력해주세요.");
     return false;
   }
   if (password !== passwordConfirm) {
-    alert("비밀번호가 일치하지 않습니다.");
+    new Toast("비밀번호가 일치하지 않습니다.");
     return false;
   }
 
@@ -54,11 +56,11 @@ export function nameValidation(name) {
   const RegExp = /^[가-힣]{2,15}$/;
 
   if (!name) {
-    alert("성함를 입력해주세요.");
+    new Toast("성함를 입력해주세요.");
     return false;
   }
   if (!RegExp.test(name)) {
-    alert("올바른 성함을 입력해주세요.");
+    new Toast("올바른 성함을 입력해주세요.");
     return false;
   }
 
@@ -69,11 +71,11 @@ export function phoneValidation(phone) {
   const RegExp = /^010[1-9][0-9]{3}[0-9]{4}$/;
 
   if (!phone) {
-    alert("휴대전화 번호를 입력해주세요.");
+    new Toast("휴대전화 번호를 입력해주세요.");
     return false;
   }
   if (!RegExp.test(phone)) {
-    alert("올바른 휴대전화 번호를 입력해주세요.");
+    new Toast("올바른 휴대전화 번호를 입력해주세요.");
     return false;
   }
 
@@ -82,7 +84,7 @@ export function phoneValidation(phone) {
 
 export function detailAddressValidation(detailAddress) {
   if (!detailAddress) {
-    alert("상세주소를 입력해주세요.");
+    new Toast("상세주소를 입력해주세요.");
     return false;
   }
 
