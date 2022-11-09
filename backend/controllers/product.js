09c420d4ddc6bdc50755fe65ffcc087c802a3c5b
Salmambo <zxcvsqwe@gmail.com>
2022-11-09 19:32:56 +0900

    fix: GET /product - getList() 삭제

--- backend/controllers/product.js
+++ backend/controllers/product.js
@@ -3,14 +3,6 @@ import { productService } from "../services";
 
 const productController = Router();
 
-productController.get("/", async (req, res, next) => {
-  try {
-    const products = await productService.getList();
-    res.status(200).json(products);
-  } catch (err) {
-    next(err);
-  }
-});
 productController.get("/:productId", async (req, res, next) => {
   try {
     const product = await productService.getProduct(req.params);
