e8531b4b102388c7305497d2149014604ec5411d
Salmambo <zxcvsqwe@gmail.com>
2022-11-09 11:31:33 +0900

    feat: conflict로 누락된 부분 추가

--- backend/db/schemas/user.js
+++ backend/db/schemas/user.js
@@ -1,32 +1,51 @@
 import { Schema } from "mongoose";
 
-const userSchema = new Schema({
-  email: {
-    type: String,
-    required: true
+const userSchema = new Schema(
+  {
+    email: {
+      type: String,
+      required: true,
+      unique: true
+    },
+    password: {
+      type: String
+    },
+    name: {
+      type: String,
+      trim: true
+    },
+    address: {
+      type: {
+        postalCode: {
+          type: String,
+          required: true,
+          default: "등록된 주소가 없습니다."
+        },
+        roadAddress: String,
+        jibunAddress: String,
+        detailAddress: String,
+        extraAddress: String
+      },
+      _id: false
+    },
+    phone: {
+      type: String
+    },
+    role: {
+      type: String,
+      required: true,
+      default: "user"
+    },
+    refreshToken: {
+      type: String,
+      required: true,
+      default: "empty"
+    }
   },
-  password: {
-    type: String
-  },
-  name: {
-    type: String
-  },
-  address: {
-    type: String
-  },
-  phone: {
-    type: String
-  },
-  role: {
-    type: String,
-    required: true,
-    default: "user"
-  },
-  refreshToken: {
-    type: String,
-    required: true,
-    default: "empty"
+  {
+    collection: "users",
+    timestamps: true
   }
-});
+);
 
 export { userSchema };
