eb8b57efdcb8b51f77fb0915f70480b89b9c1351
sjoleee <cent7425@gmail.com>
2022-11-11 05:38:14 +0900

    feat: 마이페이지 주문조회 스타일 작성

--- frontend/components/MyOrderList/MyOrderList.js
+++ frontend/components/MyOrderList/MyOrderList.js
@@ -1,61 +1,148 @@
 import { getMyOrder } from "../../apis/index.js";
 import Component from "../../core/Component.js";
+import { qs } from "../../utils/index.js";
 import MyOrderItem from "../MyOrderItem/MyOrderItem.js";
+import style from "./myOrderList.css" assert { type: "css" };
+document.adoptedStyleSheets.push(style);
 
 class MyOrderList extends Component {
   async setup() {
     const mockData = [
       {
         orderId: 1,
-        orderTime: "22-10-10",
+        createdAt: "22-10-13",
+        orderState: "입금확인",
+        totalPrice: 10000,
+        randomboxes: [
+          {
+            randomboxId: 1,
+            randomboxName: "브론즈 박스",
+            thumbnail:
+              "https://media.istockphoto.com/vectors/box-icon-on-transparent-background-vector-id1283411784?k=20&m=1283411784&s=170667a&w=0&h=bPW22hB06NP99mVas3RvvMGouiYvqd75rdcv5brSZkg=",
+            products: {
+              productId: 1,
+              productName: "청바지",
+              thumbnail:
+                "https://image.zdnet.co.kr/2022/03/03/e5c4619a509cbb6dcbd3713a6baee749.png"
+            }
+          },
+          {
+            randomboxId: 1,
+            randomboxName: "브론즈 박스",
+            thumbnail:
+              "https://media.istockphoto.com/vectors/box-icon-on-transparent-background-vector-id1283411784?k=20&m=1283411784&s=170667a&w=0&h=bPW22hB06NP99mVas3RvvMGouiYvqd75rdcv5brSZkg="
+          },
+          {
+            randomboxId: 4,
+            randomboxName: "플레티넘 박스",
+            thumbnail:
+              "https://media.istockphoto.com/vectors/box-icon-on-transparent-background-vector-id1283411784?k=20&m=1283411784&s=170667a&w=0&h=bPW22hB06NP99mVas3RvvMGouiYvqd75rdcv5brSZkg=",
+            products: {
+              productId: 1,
+              productName: "반바지",
+              thumbnail:
+                "https://image.zdnet.co.kr/2022/03/03/e5c4619a509cbb6dcbd3713a6baee749.png"
+            }
+          }
+        ]
+      },
+      {
+        orderId: 5,
+        createdAt: "22-10-10",
         orderState: "배송중",
-        orderProducts: [
+        totalPrice: 1000000,
+        randomboxes: [
           {
-            productInfo: { productId: 1, productName: "콜라", price: "1000" },
-            quantity: "1"
+            randomboxId: 1,
+            randomboxName: "브론즈 박스",
+            thumbnail:
+              "https://media.istockphoto.com/vectors/box-icon-on-transparent-background-vector-id1283411784?k=20&m=1283411784&s=170667a&w=0&h=bPW22hB06NP99mVas3RvvMGouiYvqd75rdcv5brSZkg=",
+            products: {
+              productId: 1,
+              productName: "청바지",
+              thumbnail:
+                "https://image.zdnet.co.kr/2022/03/03/e5c4619a509cbb6dcbd3713a6baee749.png"
+            }
           },
           {
-            productInfo: { productId: 2, productName: "사이다", price: "2000" },
-            quantity: "2"
+            randomboxId: 1,
+            randomboxName: "브론즈 박스",
+            thumbnail:
+              "https://media.istockphoto.com/vectors/box-icon-on-transparent-background-vector-id1283411784?k=20&m=1283411784&s=170667a&w=0&h=bPW22hB06NP99mVas3RvvMGouiYvqd75rdcv5brSZkg=",
+            products: {
+              productId: 1,
+              productName: "아이폰",
+              thumbnail:
+                "https://image.zdnet.co.kr/2022/03/03/e5c4619a509cbb6dcbd3713a6baee749.png"
+            }
           },
           {
-            productInfo: { productId: 4, productName: "주스", price: "1000" },
-            quantity: "2"
+            randomboxId: 1,
+            randomboxName: "브론즈 박스",
+            thumbnail:
+              "https://media.istockphoto.com/vectors/box-icon-on-transparent-background-vector-id1283411784?k=20&m=1283411784&s=170667a&w=0&h=bPW22hB06NP99mVas3RvvMGouiYvqd75rdcv5brSZkg=",
+            products: {
+              productId: 1,
+              productName: "맥북",
+              thumbnail:
+                "https://image.zdnet.co.kr/2022/03/03/e5c4619a509cbb6dcbd3713a6baee749.png"
+            }
           },
           {
-            productInfo: { productId: 1, productName: "몬스터", price: "3000" },
-            quantity: "10"
+            randomboxId: 1,
+            randomboxName: "브론즈 박스",
+            thumbnail:
+              "https://media.istockphoto.com/vectors/box-icon-on-transparent-background-vector-id1283411784?k=20&m=1283411784&s=170667a&w=0&h=bPW22hB06NP99mVas3RvvMGouiYvqd75rdcv5brSZkg="
+          },
+          {
+            randomboxId: 3,
+            randomboxName: "골드 박스",
+            thumbnail:
+              "https://media.istockphoto.com/vectors/box-icon-on-transparent-background-vector-id1283411784?k=20&m=1283411784&s=170667a&w=0&h=bPW22hB06NP99mVas3RvvMGouiYvqd75rdcv5brSZkg="
+          },
+          {
+            randomboxId: 4,
+            randomboxName: "플레티넘 박스",
+            thumbnail:
+              "https://media.istockphoto.com/vectors/box-icon-on-transparent-background-vector-id1283411784?k=20&m=1283411784&s=170667a&w=0&h=bPW22hB06NP99mVas3RvvMGouiYvqd75rdcv5brSZkg=",
+            products: {
+              productId: 1,
+              productName: "쿠쿠 밥솥 10000인분 조리가능",
+              thumbnail:
+                "https://image.zdnet.co.kr/2022/03/03/e5c4619a509cbb6dcbd3713a6baee749.png"
+            }
           }
         ]
       },
       {
         orderId: 2,
-        orderTime: "22-10-11",
+        createdAt: "22-10-09",
         orderState: "배송완료",
-        orderProducts: [
+        totalPrice: 100000,
+        randomboxes: [
           {
-            productInfo: {
-              productId: 4,
-              productName: "돼지고기",
-              price: "8000"
-            },
-            quantity: "1"
+            randomboxId: 1,
+            randomboxName: "브론즈 박스",
+            thumbnail:
+              "https://media.istockphoto.com/vectors/box-icon-on-transparent-background-vector-id1283411784?k=20&m=1283411784&s=170667a&w=0&h=bPW22hB06NP99mVas3RvvMGouiYvqd75rdcv5brSZkg="
           },
           {
-            productInfo: {
-              productId: 5,
-              productName: "소고기",
-              price: "50000"
-            },
-            quantity: "2"
+            randomboxId: 3,
+            randomboxName: "골드 박스",
+            thumbnail:
+              "https://media.istockphoto.com/vectors/box-icon-on-transparent-background-vector-id1283411784?k=20&m=1283411784&s=170667a&w=0&h=bPW22hB06NP99mVas3RvvMGouiYvqd75rdcv5brSZkg="
           },
           {
-            productInfo: {
-              productId: 6,
-              productName: "닭고기",
-              price: "10000"
-            },
-            quantity: "3"
+            randomboxId: 4,
+            randomboxName: "플레티넘 박스",
+            thumbnail:
+              "https://media.istockphoto.com/vectors/box-icon-on-transparent-background-vector-id1283411784?k=20&m=1283411784&s=170667a&w=0&h=bPW22hB06NP99mVas3RvvMGouiYvqd75rdcv5brSZkg=",
+            products: {
+              productId: 1,
+              productName: "청바지",
+              thumbnail:
+                "https://image.zdnet.co.kr/2022/03/03/e5c4619a509cbb6dcbd3713a6baee749.png"
+            }
           }
         ]
       }
@@ -66,13 +153,13 @@ class MyOrderList extends Component {
   }
 
   template() {
-    return `<div id="list-container"></div>`;
+    return `<div id="myorderlist-container"></div>`;
   }
 
   mounted() {
     this.state.mockData.forEach(
       (item, idx) =>
-        new MyOrderItem(this.target, {
+        new MyOrderItem(qs("#myorderlist-container"), {
           item,
           idx,
           deleteOrder: this.deleteOrder.bind(this)
--- frontend/pages/MyOrder/MyOrder.js
+++ frontend/pages/MyOrder/MyOrder.js
@@ -3,22 +3,22 @@ import Navbar from "../../components/Navbar/Navbar.js";
 import { MY_PAGE_NAV } from "../../constants/index.js";
 import Component from "../../core/Component.js";
 import { qs } from "../../utils/index.js";
+import style from "./myOrder.css" assert { type: "css" };
+document.adoptedStyleSheets.push(style);
 
 export class MyOrder extends Component {
   template() {
     return `
     <div id="mypage-nav"></div>
     <div id="mypage-container">
-      <div>
-        <span>내 주문목록</span>
-        <div id="list-container"></div>
-      </div>
+        <H1>내 주문목록</H1>
+        <div id="myorder-myorderlist-section"></div>
     </div>
     `;
   }
 
   mounted() {
     new Navbar(qs("#mypage-nav"), MY_PAGE_NAV);
-    new MyOrderList(qs("#list-container"));
+    new MyOrderList(qs("#myorder-myorderlist-section"));
   }
 }
