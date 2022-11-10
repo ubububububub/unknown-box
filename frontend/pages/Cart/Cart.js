791d19450b7f9141b9b756065dbe8667e6cd0ebd
ub <kimub1204@naver.com>
2022-11-10 15:18:43 +0900

    refactor: Cart 페이지 리팩토링
    * style 변경
    * 장바구니 비었을시 다음페이지 이동 막는 기능 추가
    * 목데이터 제거

--- frontend/pages/Cart/Cart.js
+++ frontend/pages/Cart/Cart.js
@@ -8,39 +8,6 @@ document.adoptedStyleSheets.push(style);
 
 export class Cart extends Component {
   setup() {
-    cart.setCartItem({
-      name: "MATIN FLEECE ZIP UP JUMPER IN BLACK",
-      price: 198000,
-      options: {
-        color: "orange",
-        size: "L"
-      }
-    });
-    cart.setCartItem({
-      name: "[유아인 착용 상품] 리버서블 플리스 숏 패딩 재킷 [블랙/크림] ",
-      price: 89900,
-      options: {
-        color: "yellow",
-        size: "M"
-      }
-    });
-    cart.setCartItem({
-      name: "NJ1DN82B 여성 눕시 하이브리드 다운 볼 자켓",
-      price: 249000,
-      options: {
-        color: "blue",
-        size: "M"
-      }
-    });
-    cart.setCartItem({
-      name: "[유아인 착용 상품] 리버서블 플리스 숏 패딩 재킷 [블랙/크림] ",
-      price: 89900,
-      options: {
-        color: "yellow",
-        size: "M"
-      }
-    });
-
     this.state = {
       cartList: cart.getCartList(),
       setCartList: this.setState.bind(this),
@@ -70,6 +37,12 @@ export class Cart extends Component {
       return;
     }
 
-    return (window.location = "/order/payment");
+    if (!this.isEmpty()) {
+      return (window.location = "/order/payment");
+    }
+  }
+
+  isEmpty() {
+    return this.state.cartList.length === 0;
   }
 }
